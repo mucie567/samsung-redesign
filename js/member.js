fa8446c28547d0ac4704328a3e87d329e931305c
DESKTOP-M262J75\1강의실 <mucie2159@gmail.com>
2020-11-30 15:18:53 +0900

    member js jquery add

--- js/member.js
+++ js/member.js
@@ -7,11 +7,28 @@ function focusOnFnc1(){
     });
     $('.inp-select select').on('focus',function(){
         $(this).parent().addClass('on');
-    })
+    });
     $('.inp-select select').on('blur',function(){
         $(this).parent().removeClass('on');
+    });
+    $('.input-wrap .inp-txt').on('change',function () {
+        //console.log($(this)); 
+        if ($(this).val()!='') {
+            $(this).closest('.input-wrap').addClass('valid')
+            if($(this).attr('name') == 'user-email'){
+                var v = $(this).val();
+                if (v.split('@').length !=2) {
+                    $(this).closest('.input-wrap').addClass('error');
+                } else {
+                    $(this).closest('.input-wrap').removeClass('error');
+                }
+            }
+        }  else {
+            $(this).closest('.input-wrap').removeClass('valid')
+        }
     })
 }
+
 $(function(){
     focusOnFnc1();
 });
